69ed545ec31180aa9d2d00b1db83b0650d07ccfb
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-09 15:12:52 +0100

    secondo esercizio fatto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var studente = {
   nome: "Marco",
   cognome: "Rossi",
-  eta: "25",
+  eta: 25,
 };
 
 console.log(studente);
@@ -13,5 +13,45 @@ for (var key in studente) {
 }
 
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+var studenti = [
+  {
+    nome: "Flavio",
+    cognome: "Servanti",
+    classe: 21,
+    eta: 26,
+  },
+  {
+    nome: "Maurizio",
+    cognome: "Vialli",
+    classe: 22,
+    eta: 20,
+  },
+  {
+    nome: "Vincenzo",
+    cognome: "Chiasso",
+    classe: 21,
+    eta: 22,
+  },
+  {
+    nome: "Giovanni",
+    cognome: "Bassi",
+    classe: 20,
+    eta: 28,
+  },
+  {
+    nome: "Dario",
+    cognome: "Balestri",
+    classe: 21,
+    eta: 26,
+  },
+];
+
+//ciclo for per stampare nome e cognome di tutti gli studenti
+
+for (var chiave in studenti) {
+  console.log(studenti[chiave].nome + " " + studenti[chiave].cognome);
+}
+
 // - Dare la possibilità all’utente attraverso 3 prompt di
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
